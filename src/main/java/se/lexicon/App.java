2f9dc1094228bd161afb5e0a1b87e40111088226
aman adinlid <95pinkpanda@gmail.com>
2021-02-24 16:27:56 +0100

    Tested part 2..

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,9 +1,12 @@
 package se.lexicon;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.Config.InputServiceConfig;
 import se.lexicon.Config.StudentConfig;
 import se.lexicon.Dao.StudentDao;
 import se.lexicon.Model.Student;
+import se.lexicon.Util.ScannerInputService;
+import se.lexicon.Util.UserInputService;
 
 public class App {
     public static void main(String[] args) {
@@ -19,6 +22,9 @@ public class App {
         System.out.println("-------------------------");
         boolean remove = dao.delete(1);
         System.out.println("remove = " + remove);
+        System.out.println("_____________________________");
+        AnnotationConfigApplicationContext context1 = new AnnotationConfigApplicationContext(InputServiceConfig.class);
+        UserInputService userInputService= context.getBean(UserInputService.class);
     }
 
 }
--- src/main/java/se/lexicon/Config/InputServiceConfig.java
+++ src/main/java/se/lexicon/Config/InputServiceConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import se.lexicon.Util.ScannerInputService;
 import se.lexicon.Util.UserInputService;
 
+import java.util.Scanner;
+
 @Configuration
 public class InputServiceConfig {
 
     @Bean("ScannerInputService")
 
-    public UserInputService userInputService() {
-        return new ScannerInputService();
+    public Scanner scanner() {
+        return new Scanner(System.in);
     }
 
 }
